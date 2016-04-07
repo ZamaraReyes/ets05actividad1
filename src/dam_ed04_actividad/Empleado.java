0067fcb3637700845bf1f3107d4300be24bf45fa
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-08 00:36:16 +0100

    commit cuarto

---
+++ src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+
+public class Empleado {
+    
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    
+}
